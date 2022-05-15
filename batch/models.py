7be9801a238552e53f4cb01c50cec9a942f7cd63
sudo-rgorai <rahul.gorai.3141@gmail.com>
2022-05-15 21:51:21 +0530

    Add password for profile

--- batch/models.py
+++ batch/models.py
@@ -14,6 +14,7 @@ class Profile(models.Model):
     image = models.ImageField(upload_to=nameFile, blank=True, null=True)
     subscription_start_date=models.DateField()
     subscription_end_date=models.DateField()
+    password=models.CharField(max_length=50)
     def __str__(self):
         return self.name
 
