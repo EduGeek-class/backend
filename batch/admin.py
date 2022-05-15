884cb18fa8edb1d2e82904af36eed130d4f18287
sudo-rgorai <rahul.gorai.3141@gmail.com>
2022-05-15 13:33:57 +0530

    Add admin model

--- batch/admin.py
+++ batch/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-from .models import Profile,Courses,Batches
-admin.register(Profile, Courses,Batches)(admin.ModelAdmin)
+from .models import Profile, Courses, Batches, Admin
+admin.register(Profile, Courses, Batches, Admin)(admin.ModelAdmin)
