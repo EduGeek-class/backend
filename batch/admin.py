6b002af3b076346da15662eca6413ddee03b81a4
iamananya <ananyaiit123@gmail.com>
2022-10-03 18:29:58 +0530

    files updated

--- batch/admin.py
+++ batch/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 from .models import Profile, Courses,Subjects, Batches, Admin,StudyMaterial,Notification
 admin.register(Profile, Courses,StudyMaterial, Subjects,Batches, Admin ,Notification)(admin.ModelAdmin)
+
+# To make changes in the django admin
